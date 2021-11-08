24654feca3aa51c87833d014e228d2d0cb545767
Dennie Walo <dennie.walo@gmail.com>
2021-11-08 14:05:34 +0100

    todos can be added and removed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+//todo array
+var todos = [];
+var nextId = 0;
+
 //use static style
 app.use("/static", express.static("public"));
 //set view engine to ejs
@@ -10,13 +14,31 @@ app.use(express.urlencoded({ extended: true }));
 
 //routing
 app.get('/', (req, res) => {
-    res.render('todo.ejs');
+    res.render('todo.ejs', {todos});
 });
 
-app.post('/',(req, res) => {
-    console.log(req.body);
+app.post('/add',(req, res) => {
+    if(req.body.content != ''){
+      todos.push({
+        id: nextId,
+        content: req.body.content,
+      });
+      console.log(todos);
+      nextId++;
+    }
+    res.redirect("/");
+});
+
+app.get('/remove', (req, res) => {
+  todos.forEach(todo => {
+    console.log(todo.id);
+    if(todo.id == Number(req.query.id)){
+      todos.pop(todo);
+    }
+  })
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
-  console.log(`Example app listening at http://localhost:3000`);
+  console.log("App is listening at http://localhost:3000");
 });
